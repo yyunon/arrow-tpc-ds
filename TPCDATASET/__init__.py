6e2f61a5d7c5bb47be7750ff440eb6e8d8124351
yuksel <yonsel.yuksel@gmail.com>
2021-04-30 19:16:07 +0200

    Add tpch suite wrapper: In future there will be a call to tpch suite for dataset gen.

--- TPCDATASET/__init__.py
+++ TPCDATASET/__init__.py
@@ -1,3 +1,4 @@
 import TPCDATASET.dataset
 import TPCDATASET.tpc_ds
 import TPCDATASET.tpc_h
+import TPCDATASET.tpch_suite
---
+++ TPCDATASET/tpch_suite.py
@@ -0,0 +1,22 @@
+from ctypes import *
+from subprocess import *
+import pathlib
+
+
+def dbgen():
+    tpchkit = ImportModules("tpch")
+    dbgen_functor = tpchkit.dataset_generator
+    print(dbgen_functor.main(2, "-h"))
+
+
+class ImportModules:
+    def __init__(self, module):
+        self.so = None
+        if module == "tpch":
+            self.so = "/usr/local/lib/dbgen.so"
+        else:
+            print("TODO: Make this intelligent")
+            print("Error: You did not install tpch-kit, exiting...")
+            assert(False)
+
+        self.dataset_generator = CDLL(self.so)
